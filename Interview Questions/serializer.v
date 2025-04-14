7a610c938b763f0376ae8270d54a598375cecffd
darmar796 <65272260+darmar796@users.noreply.github.com>
2025-04-13 23:53:03 -0400

    Update serializer.v

--- Interview Questions/serializer.v
+++ Interview Questions/serializer.v
@@ -1,3 +1,10 @@
+// serializer
+// parallel data in, serial data out
+// din_en loads a new value from din
+// loaded value is shifted starting from lsb
+// when done shifting the dout is 0
+// when in reset, loaded value is set to 0
+
 module model #(parameter
   DATA_WIDTH = 16
 ) (
@@ -8,8 +15,8 @@ module model #(parameter
   output logic dout
 );
 
-reg [DATA_WIDTH-1:0] buffer;
-assign dout = buffer[0];
+reg [DATA_WIDTH-1:0] buffer; //buffer to hold the din
+assign dout = buffer[0]; // output is lsb
 
 always @ (posedge clk) begin
   if (~resetn) begin // synchrounous active low reset
@@ -19,8 +26,8 @@ always @ (posedge clk) begin
     if (din_en) begin // load state
       buffer <= din; // load new din
     end
-    else begin  // e.g. din < dout  // the value was smaller than the current 2nd largest value
-      buffer <= buffer >> 1;  // shift right with 0 pad
+    else begin  // shift state
+      buffer <= buffer >> 1;  // bit-shift right with 0 pad, update buffer
     end
   end
 end
